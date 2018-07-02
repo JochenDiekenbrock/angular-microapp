c6fe8a0bde811c63735e375972e714d78462cb5e
Jochen Diekenbrock <jochen.diekenbrock@tui.com>
2018-07-02 16:41:12 +0200

    move navigation handling from ShellComponent to AppComponent, because the router subscription in the ShellComponent is not enabled timingly if using deep links (for example http://127.0.0.1:4200/client-a/page2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavigationEnd, NavigationStart, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {StateService} from './state.service';
+import {LoadService} from './load.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  subscription: Subscription;
+
+  constructor(private router: Router, private loadService: LoadService, private stateService: StateService) {
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url.indexOf('client-a') >= 0) {
+          this.loadService.load('client-a');
+        }
+        if (event.url.indexOf('client-b') >= 0) {
+          this.loadService.load('client-b');
+        }
+
+        this.stateService.setUrl(event.url);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/shell.component.ts
+++ src/app/shell.component.ts
@@ -8,29 +8,5 @@ import {StateService} from './state.service';
   selector: 'app-shell',
   templateUrl: './shell.component.html'
 })
-export class ShellComponent implements OnInit, OnDestroy {
-  subscription: Subscription;
-
-  constructor(private router: Router, private loadService: LoadService, private stateService: StateService) {
-  }
-
-  ngOnInit(): void {
-    this.subscription = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        if (event.url.indexOf('client-a') >= 0) {
-          this.loadService.load('client-a');
-        }
-        if (event.url.indexOf('client-b') >= 0) {
-          this.loadService.load('client-b');
-        }
-
-        this.stateService.setUrl(event.url);
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
+export class ShellComponent {
 }
